252d656f8a3d0cc983cfd25d3678f55b4899642e
kalderlunt <kalderlunt@gmail.com>
2024-10-27 01:46:19 +0200

    fix convoyer lineRenderer
    startPoint -> endPoint

--- Factorio/Assets/Runtime/Scripts/Convoyer with line renderer/TEST_ConveyorBelt.cs
+++ Factorio/Assets/Runtime/Scripts/Convoyer with line renderer/TEST_ConveyorBelt.cs
@@ -10,7 +10,7 @@ namespace Test
         {
             public Transform item;
             [HideInInspector] public float currentLerp;
-            [HideInInspector] public int startPoint;
+            [HideInInspector] public int endPoint = 0;
         }
 
         [SerializeField] private float _itemSpacing;
@@ -18,7 +18,7 @@ namespace Test
         [SerializeField] private LineRenderer _lineRenderer;
         [SerializeField] private List<ConveyorBeltItem> _items;
 
-        private void FixedUpdate()
+        private void Update()
         {
             for (int i = 0; i < _items.Count; i++)
             {
@@ -29,16 +29,16 @@ namespace Test
                     if (Vector3.Distance(item.position, _items[i - 1].item.position) <= _itemSpacing)
                         continue;
 
-                item.transform.position = Vector3.Lerp(_lineRenderer.GetPosition(beltItem.startPoint), _lineRenderer.GetPosition(beltItem.startPoint + 1), beltItem.currentLerp);
-                float distance = Vector3.Distance(_lineRenderer.GetPosition(beltItem.startPoint), _lineRenderer.GetPosition(beltItem.startPoint + 1));
-                beltItem.currentLerp += (_speed * Time.fixedDeltaTime) / distance;
+                item.transform.position = Vector3.Lerp(_lineRenderer.GetPosition(beltItem.endPoint - 1), _lineRenderer.GetPosition(beltItem.endPoint), beltItem.currentLerp);
+                float distance = Vector3.Distance(_lineRenderer.GetPosition(beltItem.endPoint - 1), _lineRenderer.GetPosition(beltItem.endPoint));
+                beltItem.currentLerp += (_speed * Time.deltaTime) / distance;
 
                 if (beltItem.currentLerp >= 1)
                 {
-                    if (beltItem.startPoint + 2 < _lineRenderer.positionCount)
+                    if (beltItem.endPoint + 1 < _lineRenderer.positionCount)
                     {
                         beltItem.currentLerp = 0;
-                        beltItem.startPoint++;
+                        beltItem.endPoint++;
                     }
                 }
             }
